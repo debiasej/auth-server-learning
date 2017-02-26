9bc42ff9ce0d2321c31fcebb2c03a5a31eccb0a4
Mario de Biase <debiasej@gmail.com>
2017-02-26 20:48:41 +0100

    Note about async/await feature

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,8 +1,18 @@
+//const async = require('asyncawait/async');
+//const await = require('asyncawait/await');
 const User = require('../models/user');
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const pass = req.body.password;
+
+  // Using async/await feature is cleaner but now it isn't available yet.
+  // A workaround is use asyncawait library but then the code isn't clean.
+  // (async (function findUser() {
+  //   var resp = await (User.findOne({ email: email }));
+  //   console.log("await:");
+  //   console.log(resp);
+  // }))();
 
   // See if a user with a given email exists
   User.findOne({ email: email })
