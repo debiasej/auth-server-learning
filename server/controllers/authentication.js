34c8e7b14e4c5c871d54b37bc7de8832e447afff
Mario de Biase <debiasej@gmail.com>
2017-02-26 20:41:49 +0100

    Refactor: signup with promises

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -5,26 +5,29 @@ exports.signup = function(req, res, next) {
   const pass = req.body.password;
 
   // See if a user with a given email exists
-  User.findOne({ email: email }, function(err, existingUser) {
-    if (err) { return next(err); }
+  User.findOne({ email: email })
+  .then(function(existingUser) {
 
     // If a user with email does exist, return a error
     if (existingUser) {
       // 422 Unprocessable Entity
       return res.status(422).send({ error: 'Email is in use' });
     }
+    // If an user with email doesn't exist, create and save user record
+    createAndStoreUser()
 
-    // If a a user with email doesn't exist, create and save user record
-    const user = new User({
-      email: email,
-      password: pass
-    });
+  })
+  .catch(function(err){
+    return next(err);
+  });
+
+  function createAndStoreUser () {
+    const user = new User({ email: email, password: pass });
 
     user.save(function(err) {
       if (err) { return next(err); }
-
       // Respond to request indicating the user was created
-      res.json(user);
+      res.json({ success: true });
     });
-  });
+  }
 }
