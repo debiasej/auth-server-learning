e65192a37aa3f509ece545377bb940621a086908
Mario de Biase <debiasej@gmail.com>
2017-02-26 20:20:51 +0100

    Signup a user with callbacks

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,30 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
+  const email = req.body.email;
+  const pass = req.body.password;
+
+  // See if a user with a given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
+
+    // If a user with email does exist, return a error
+    if (existingUser) {
+      // 422 Unprocessable Entity
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    // If a a user with email doesn't exist, create and save user record
+    const user = new User({
+      email: email,
+      password: pass
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      // Respond to request indicating the user was created
+      res.json(user);
+    });
+  });
 }
