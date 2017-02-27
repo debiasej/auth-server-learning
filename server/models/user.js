522b1ec3f15d9b9406f7bb7e0c62b95cd73476c8
Mario de Biase <debiasej@gmail.com>
2017-02-27 10:06:00 +0100

    Salt passwords using Bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcript = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// Encript password
+// 'Pre' means that before saving a model, run this function.
+userSchema.pre('save', function(next){
+  const user = this; // user model
+
+  // Generate a salt
+  bcript.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    // Hash our password using the salt
+    bcript.hash(user.password, salt, null, function(err, hash) {
+      if(err) { return next(err); }
+
+      // Override plain password with the encripted one.
+      user.password = hash;
+      next();
+    });
+  })
 });
 
 // Create the model class
