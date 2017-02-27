9bdb6cc60b0dd421f257b4fc07f2c6f5c5bb2ba9
Mario de Biase <debiasej@gmail.com>
2017-02-27 14:43:06 +0100

    Function to compare a pass and stored hash

--- server/models/user.js
+++ server/models/user.js
@@ -33,6 +33,14 @@ function encryptPass(user, next) {
   });
 }
 
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) { return callback(err); }
+
+    callback(null, isMatch);
+  });
+}
+
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
 
