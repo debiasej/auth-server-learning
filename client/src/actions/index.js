22e6c184c89aaad8f90520717dabedadd7a937c0
Mario de Biase <debiasej@gmail.com>
2017-03-04 11:25:29 +0100

    SignIn and authenticate an email correctly

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,23 +1,28 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const API_URL = 'http://localhost:3000';
-
 
 export function signinUser({email, password}) {
   return function(dispatch) {
       // Submit email/password to the server using ES6 syntax to post data
-      axios.post(`${API_URL}/signin`, { email, password });
-
+      axios.post(`${API_URL}/signin`, { email, password })
+        .then(response => {
+          // If request is good...
+          // - Update state to indicate user is authenticated
+          dispatch({ type: AUTH_USER });
+          // - Save the JWT token
+          localStorage.setItem('token', response.data.token);
+          // - Redirect to the route '/feature'
+          browserHistory.push('/feature');
+        })
+        .catch( () => {
+          // If request is bad
+          // - Show an error to the server
+        });
   }
 }
-
-  // If request is good...
-  // - Update state to indicate user is authenticated
-  // - Save the JWT token
-  // - Redirect to the route '/feature'
-
-  // If request is bad
-  // - Show an error to the server
 
 // Note: react-promise is not exactly what we want beacause of
 // we need to handle different side effect depending on the server response.
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
+import authReducer from './auth_reducer';
+
+debugger;
 
 const rootReducer = combineReducers({
   //{form: form} ~ {form} ES6 syntax
-  form
+  form,
+  auth: authReducer
 });
 
 export default rootReducer;
