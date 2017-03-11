f944a56e05d583aba392dc95301b498259127152
Mario de Biase <debiasej@gmail.com>
2017-03-11 21:01:15 +0100

    Add form validation

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -4,30 +4,44 @@ import { reduxForm, Field } from 'redux-form';
 import * as actions from '../../actions';
 
 class Signup extends Component {
+
+  renderField = ({ input, label, type, meta: { touched, error } }) => (
+      <fieldset className="form-group">
+        <label>{label}</label>
+        <input className="form-control" {...input} placeholder={label} type={type}/>
+        {touched && (error && <div className="error">{error}</div>)}
+      </fieldset>
+    );
+
   render() {
     const { handleSubmit } = this.props;
+
     return(
       <form>
-      <fieldset className="form-group">
-        <label>Email:</label>
-        <Field className="form-control" name="email" component="input" type="text" />
-      </fieldset>
-      <fieldset className="form-group">
-        <label>Password:</label>
-        <Field className="form-control" name="password" component="input" type="password" />
-      </fieldset>
-      <fieldset className="form-group">
-        <label>Confirm Password:</label>
-        <Field className="form-control" name="confirmpassword" component="input" type="password" />
-      </fieldset>
-      <button className="btn btn-primary" action="submit">Sign up</button>
+        <Field name="email" type="text" label="Email:" component={this.renderField} />
+        <Field name="password" type="password" label="Password:" component={this.renderField} />
+        <Field name="confirmpassword" type="password" label="Confirm Password:" component={this.renderField} />
+        <button className="btn btn-primary" action="submit">Sign up</button>
       </form>
     );
   }
 }
 
+function validate(formProps) {
+  const errors = {};
+
+  if (formProps.password !== formProps.confirmpassword) {
+    errors.password = 'Passwords must match';
+  }
+
+  return errors;
+}
+
 // Decorate the form component
-Signup = reduxForm({ form: 'signup' })(Signup);
+Signup = reduxForm({
+  form: 'signup',
+  validate
+ })(Signup);
 
 // Babel fails when transpile variables using '* as ... from'
 // Remove the property __esModule from actions
